1b845323e0db0f1dd8c059b2f9a025422318843a
tuyen <0308191094@caothang.edu.vn>
2022-05-31 09:09:41 +0700

    KTS1_QuangTuyen

---
+++ KTS1_Quang Tuyen/app/src/main/java/com/example/kt_th1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kt_th1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    Intent i;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void xulyThoat (View view) {
+       finish();
+   }
+
+}
